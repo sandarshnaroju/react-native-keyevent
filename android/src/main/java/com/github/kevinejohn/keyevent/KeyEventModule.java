9647d0611ca038e25d11c458c177a156044e7f23
Sandarsh Naroju <narojusandarsh18@gmail.com>
2020-10-07 13:48:20 +0530

    added button keys for set top box

--- android/src/main/java/com/github/kevinejohn/keyevent/KeyEventModule.java
+++ android/src/main/java/com/github/kevinejohn/keyevent/KeyEventModule.java
@@ -9,6 +9,11 @@ import com.facebook.react.bridge.WritableMap;
 
 import android.view.KeyEvent;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 /**
  * Created by Kevin Johnson on 8/15/16.
  */
@@ -93,5 +98,27 @@ public class KeyEventModule extends ReactContextBaseJavaModule {
     @Override
     public String getName() {
         return "KeyEventModule";
+    }
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        constants.put("KEYCODE_0", KeyEvent.KEYCODE_0);
+        constants.put("KEYCODE_1", KeyEvent.KEYCODE_1);
+        constants.put("KEYCODE_2", KeyEvent.KEYCODE_2);
+        constants.put("KEYCODE_3", KeyEvent.KEYCODE_3);
+        constants.put("KEYCODE_4", KeyEvent.KEYCODE_4);
+        constants.put("KEYCODE_5", KeyEvent.KEYCODE_5);
+        constants.put("KEYCODE_6", KeyEvent.KEYCODE_6);
+        constants.put("KEYCODE_7", KeyEvent.KEYCODE_7);
+        constants.put("KEYCODE_8", KeyEvent.KEYCODE_8);
+        constants.put("KEYCODE_9", KeyEvent.KEYCODE_9);
+        constants.put("KEYCODE_DPAD_UP", KeyEvent.KEYCODE_DPAD_UP);
+        constants.put("KEYCODE_DPAD_DOWN", KeyEvent.KEYCODE_DPAD_DOWN);
+        constants.put("KEYCODE_DPAD_LEFT", KeyEvent.KEYCODE_DPAD_LEFT);
+        constants.put("KEYCODE_DPAD_RIGHT", KeyEvent.KEYCODE_DPAD_RIGHT);
+        constants.put("KEYCODE_DPAD_CENTER", KeyEvent.KEYCODE_DPAD_CENTER);
+        constants.put("KEYCODE_BACK", KeyEvent.KEYCODE_BACK);
+        constants.put("KEYCODE_GUIDE", KeyEvent.KEYCODE_GUIDE);
+        return constants;
     }
 }
--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-import { DeviceEventEmitter, NativeEventEmitter, NativeModules, Platform } from 'react-native';
+import {
+  DeviceEventEmitter,
+  NativeEventEmitter,
+  NativeModules,
+  Platform,
+} from "react-native";
+
+export const KEYCODE_0 = NativeModules.KeyEventModule.KEYCODE_0;
+export const KEYCODE_1 = NativeModules.KeyEventModule.KEYCODE_1;
+export const KEYCODE_2 = NativeModules.KeyEventModule.KEYCODE_2;
+export const KEYCODE_3 = NativeModules.KeyEventModule.KEYCODE_3;
+export const KEYCODE_4 = NativeModules.KeyEventModule.KEYCODE_4;
+export const KEYCODE_5 = NativeModules.KeyEventModule.KEYCODE_5;
+export const KEYCODE_6 = NativeModules.KeyEventModule.KEYCODE_6;
+export const KEYCODE_7 = NativeModules.KeyEventModule.KEYCODE_7;
+export const KEYCODE_8 = NativeModules.KeyEventModule.KEYCODE_8;
+export const KEYCODE_9 = NativeModules.KeyEventModule.KEYCODE_9;
+export const KEYCODE_DPAD_UP = NativeModules.KeyEventModule.KEYCODE_DPAD_UP;
+export const KEYCODE_DPAD_DOWN = NativeModules.KeyEventModule.KEYCODE_DPAD_DOWN;
+export const KEYCODE_DPAD_LEFT = NativeModules.KeyEventModule.KEYCODE_DPAD_LEFT;
+export const KEYCODE_DPAD_RIGHT =
+  NativeModules.KeyEventModule.KEYCODE_DPAD_RIGHT;
+export const KEYCODE_DPAD_CENTER =
+  NativeModules.KeyEventModule.KEYCODE_DPAD_CENTER;
+export const KEYCODE_BACK = NativeModules.KeyEventModule.KEYCODE_BACK;
+export const KEYCODE_GUIDE = NativeModules.KeyEventModule.KEYCODE_GUIDE;
 
 class KeyEvent {
   onKeyDownListener(cb) {
     this.removeKeyDownListener();
     if (Platform.OS === "ios") {
       let keyEvent = new NativeEventEmitter(NativeModules.RNKeyEvent);
-      this.listenerKeyDown = keyEvent.addListener('onKeyDown', cb);
+      this.listenerKeyDown = keyEvent.addListener("onKeyDown", cb);
     } else {
-      this.listenerKeyDown = DeviceEventEmitter.addListener('onKeyDown', cb);
+      this.listenerKeyDown = DeviceEventEmitter.addListener("onKeyDown", cb);
     }
   }
 
@@ -22,9 +47,9 @@ class KeyEvent {
     this.removeKeyUpListener();
     if (Platform.OS === "ios") {
       let keyEvent = new NativeEventEmitter(NativeModules.RNKeyEvent);
-      this.listenerKeyUp = keyEvent.addListener('onKeyUp', cb);
+      this.listenerKeyUp = keyEvent.addListener("onKeyUp", cb);
     } else {
-      this.listenerKeyUp = DeviceEventEmitter.addListener('onKeyUp', cb);
+      this.listenerKeyUp = DeviceEventEmitter.addListener("onKeyUp", cb);
     }
   }
 
@@ -39,9 +64,12 @@ class KeyEvent {
     this.removeKeyMultipleListener();
     if (Platform.OS === "ios") {
       let keyEvent = new NativeEventEmitter(NativeModules.RNKeyEvent);
-      this.listenerKeyMultiple = keyEvent.addListener('onKeyMultiple', cb);
+      this.listenerKeyMultiple = keyEvent.addListener("onKeyMultiple", cb);
     } else {
-      this.listenerKeyMultiple = DeviceEventEmitter.addListener('onKeyMultiple', cb);
+      this.listenerKeyMultiple = DeviceEventEmitter.addListener(
+        "onKeyMultiple",
+        cb
+      );
     }
   }
 
